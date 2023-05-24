a714143ea859d9ebe81c1131bab86d11aa5656a5
Phillister-yekebedi <otienoyekebedi@gmail.com>
2023-05-24 17:29:37 +0300

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ document.body.style.backgroundColor= 'silver'
 
 document.getElementById('title').style.color = 'green'
 
-let allH3Elements= document.querySelector('h3')
-allH3Elements.style.textTransform = 'uppercase';
+// let allH3Elements= document.querySelector('h3')
+// allH3Elements.style.textTransform = 'uppercase';
+
+let allH3Elements= document.querySelectorAll('h3')
+for(let y = 0;y < allH3Elements.length;y++){
+    allH3Elements[y].style.textTransform= 'uppercase'
+}
 
 
 
-// let tit = document.createElement(''li);
-// h2.innerHTML = "I am a graduate student";
-// document.getElementById('container').appendChild(h2);
+let oldfrut = document.getElementById('fruList');
+let newList = document.createElement('li');
+let newFruit = document.createTextNode('grapes')
+newList.appendChild(newFruit);
+oldfrut.appendChild(newList)
+
+
+let oldVegies = document.getElementById('vegList');
+let newVegies= document.createElement('li');
+let newVegesName= document.createTextNode('Kunde')
+newVegies.appendChild(newVegesName);
+oldVegies.appendChild(newVegies);
